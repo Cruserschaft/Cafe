2d2b9645f01f32f6acd83416bf85e515fe76e016
Cruserschaft <50294166+Cruserschaft@users.noreply.github.com>
2022-09-19 20:11:37 +0300

    first commit cafe site (add migration)

---
+++ main/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class DishType(models.Model):
+    dish_type_name = models.TextField()
+
+
+class Menu(models.Model):
+    dish_name = models.TextField()
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    dish_comp = models.TextField(blank=True)
+    dish_cost = models.PositiveIntegerField()
+
+
+class Events(models.Model):
+    event_title = models.TextField()
+    event_cost = models.PositiveIntegerField()
+    event_about_start = models.TextField()
+    event_about1 = models.TextField(blank=True)
+    event_about2 = models.TextField(blank=True)
+    event_about3 = models.TextField(blank=True)
+    event_about_finish = models.TextField(blank=True)
+
+
+class Specials(models.Model):
+    specials_name = models.TextField()
+    specials_title = models.TextField()
+    specials_about1 = models.TextField()
+    specials_about2 = models.TextField(blank=True)
+    specials_image = models.ImageField(upload_to="images")
+
+
+class About(models.Model):
+    about_title = models.TextField()
+    about_start_text = models.TextField()
+    about_p1 = models.TextField()
+    about_p2 = models.TextField(blank=True)
+    about_p3 = models.TextField(blank=True)
+    about_end = models.TextField(blank=True)
+
+
