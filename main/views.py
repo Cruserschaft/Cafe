14df30915d38d0ddff5c9917839cc34d282e7345
Cruserschaft <50294166+Cruserschaft@users.noreply.github.com>
2022-09-25 13:10:50 +0300

    correct, start manager

--- main/views.py
+++ main/views.py
@@ -5,11 +5,11 @@ from .models import DishType, Menu
 
 def start(request):
     categories = DishType.objects.filter(dish_type_access=True)
-    dishes = Menu.objects.filter(dish_is_special=False)
-    dishes_specials = Menu.objects.filter(dish_is_special=True)
+    dishes = Menu.objects.filter(dish_is_special=False, dish_access=True)
+    dishes_specials = Menu.objects.filter(dish_is_special=True, dish_access=True)
 
     return render(request, "main.html", context={
         "category": categories,
         "dishes": dishes,
-        "dishes_specials": dishes_specials,
+        "special_dishes": dishes_specials,
     })
