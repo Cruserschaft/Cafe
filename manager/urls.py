39649c4a206a1b9c68461c305fc5458d3ef395b5
Cruserschaft <50294166+Cruserschaft@users.noreply.github.com>
2022-10-02 12:05:43 +0300

    update reservations

--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import *
 
+app_name = "manager"
+
 urlpatterns = [
-    path("", reservation_list),
+    path("", reservation_list, name="reservation_list"),
+    path("update/<int:pk>/", reservation_update, name="reservation_close"),
+
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import UserReservations
 
 
+def is_manager(user):
+    return user.groups.filter(name="manager").exists()
+
+
 @login_required(login_url="/login/")
+@user_passes_test(is_manager)
 def reservation_list(request):
     lst = UserReservations.objects.filter(is_processed=False)
     return render(request, "reservation_list.html", context={"lst":lst})
+
+@login_required(login_url="/login/")
+@user_passes_test(is_manager)
+def reservation_update(request, pk):
+    UserReservations.objects.filter(pk=pk).update(is_processed=True)
+    return redirect("manager:reservation_list")
